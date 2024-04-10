fbc672c2af72ce0f4a8c68b0a11cbd405ccd3b29
alex <volohovaleksandr1974@gmail.com>
2024-04-10 23:44:17 +0300

    lab2 upd

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,16 +1,16 @@
 import glfw
 from OpenGL.GL import *
-from math import cos, sin, sqrt, asin
+from math import cos, sin
 
-alpha = 0.0
-beta = 0.0
+a = 0.0
+b = 0.0
 fill = True
 
 
 def main():
     if not glfw.init():
         return
-    window = glfw.create_window(640, 640, "LAB 2", None, None)
+    window = glfw.create_window(700, 700, "CUBE", None, None)
     if not window:
         glfw.terminate()
         return
@@ -32,101 +32,99 @@ def display(window):
     glClear(GL_DEPTH_BUFFER_BIT)
     glMatrixMode(GL_PROJECTION)
 
-    # move
+    # смещение
     glMultMatrixf([1, 0, 0, 0,
                    0, 1, 0, 0,
                    0, 0, 1, 0,
                    0.75, 0.75, 0, 1])
 
-    fz = 0
-    theta = 0.61557763
-    phi = 0.785398
+    t = -0.4
+    p = 0.463648
 
-    def projection():
+    def Proj():  # кабинетная проекция
         glMultMatrixf([
-            cos(phi), sin(phi) * sin(theta), sin(theta) * cos(theta), 0,
-            0, cos(theta), -sin(theta), 0,
-            sin(phi), -cos(phi) * sin(theta), -cos(phi) * cos(theta), 0,
+            cos(p), 0, sin(p), 0,
+            0, cos(p), -cos(p) * sin(t), 0,
+                       sin(p) * cos(t), -sin(t), -cos(t) * cos(p), 0,
             0, 0, 0, 1,
         ])
 
-    projection()
+    Proj()
 
-    def cube(sz):
+    def cube(x):
+        x = x / 2
         glBegin(GL_QUADS)
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3f(-sz / 2, -sz / 2, -sz / 2)
-        glVertex3f(-sz / 2, sz / 2, -sz / 2)
-        glVertex3f(-sz / 2, sz / 2, sz / 2)
-        glVertex3f(-sz / 2, -sz / 2, sz / 2)
-        glColor3f(1.0, 0.0, 0.0);
-        glVertex3f(sz / 2, -sz / 2, -sz / 2)
-        glVertex3f(sz / 2, -sz / 2, sz / 2)
-        glVertex3f(sz / 2, sz / 2, sz / 2)
-        glVertex3f(sz / 2, sz / 2, -sz / 2)
-        glColor3f(0.0, 1.0, 0.0);
-        glVertex3f(-sz / 2, -sz / 2, -sz / 2)
-        glVertex3f(-sz / 2, -sz / 2, sz / 2)
-        glVertex3f(sz / 2, -sz / 2, sz / 2)
-        glVertex3f(sz / 2, -sz / 2, -sz / 2)
-        glColor3f(1.0, 1.0, 0.0);
-        glVertex3f(-sz / 2, sz / 2, -sz / 2)
-        glVertex3f(-sz / 2, sz / 2, sz / 2)
-        glVertex3f(sz / 2, sz / 2, sz / 2)
-        glVertex3f(sz / 2, sz / 2, -sz / 2)
-        glColor3f(0.0, 1.0, 1.0);
-        glVertex3f(-sz / 2, -sz / 2, -sz / 2)
-        glVertex3f(sz / 2, -sz / 2, -sz / 2)
-        glVertex3f(sz / 2, sz / 2, -sz / 2)
-        glVertex3f(-sz / 2, sz / 2, -sz / 2)
-        glColor3f(1.0, 0.0, 1.0);
-        glVertex3f(-sz / 2, -sz / 2, sz / 2)
-        glVertex3f(sz / 2, -sz / 2, sz / 2)
-        glVertex3f(sz / 2, sz / 2, sz / 2)
-        glVertex3f(-sz / 2, sz / 2, sz / 2)
+        glColor3f(1.0, 0.0, 1.0)
+        glVertex3f(-x, -x, -x)
+        glVertex3f(-x, x, -x)
+        glVertex3f(-x, x, x)
+        glVertex3f(-x, -x, x)
+        glColor3f(1.0, 0.5, 0.5)
+        glVertex3f(x, -x, -x)
+        glVertex3f(x, -x, x)
+        glVertex3f(x, x, x)
+        glVertex3f(x, x, -x)
+        glColor3f(0.5, 1.0, 0.0)
+        glVertex3f(-x, -x, -x)
+        glVertex3f(-x, -x, x)
+        glVertex3f(x, -x, x)
+        glVertex3f(x, -x, -x)
+        glColor3f(1.0, 1.0, 0.0)
+        glVertex3f(-x, x, -x)
+        glVertex3f(-x, x, x)
+        glVertex3f(x, x, x)
+        glVertex3f(x, x, -x)
+        glColor3f(0.0, 1.0, 1.0)
+        glVertex3f(-x, -x, -x)
+        glVertex3f(x, -x, -x)
+        glVertex3f(x, x, -x)
+        glVertex3f(-x, x, -x)
+        glColor3f(1.0, 0.5, 0.0)
+        glVertex3f(-x, -x, x)
+        glVertex3f(x, -x, x)
+        glVertex3f(x, x, x)
+        glVertex3f(-x, x, x)
         glEnd()
 
-    cube(0.2)
+    cube(0.25)
 
     glLoadIdentity()
 
-    global alpha
-    global beta
-    x = 0.7
-    y = 0.7
-    fz = sqrt(x * x + y * y)
-    theta = asin(fz / sqrt(2)) + alpha
-    phi = asin(fz / sqrt(2 - fz * fz)) + beta
-    projection()
+    global a
+    global b
+    t = -0.4 + a
+    p = 0.463648 + b
 
-    cube(0.7)
+    Proj()
+
+    cube(0.8)
 
     glfw.swap_buffers(window)
     glfw.poll_events()
 
 
 def key_callback(window, key, scancode, action, mods):
-    global alpha
-    global beta
-    global fill
+    global a
+    global b
     if action == glfw.PRESS or action == glfw.REPEAT:
         if key == glfw.KEY_RIGHT:
-            beta += 0.1
+            b += 0.05
         elif key == glfw.KEY_LEFT:
-            beta -= 0.1
+            b -= 0.05
         elif key == glfw.KEY_UP:
-            alpha += 0.1
+            a += 0.05
         elif key == glfw.KEY_DOWN:
-            alpha -= 0.1
+            a -= 0.05
         elif key == glfw.KEY_F:
+            global fill
             fill = not fill
             if fill:
                 glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
             else:
                 glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
-        elif key == glfw.KEY_W:
-            glFrontFace(GL_CW if glGetBoolean(GL_CULL_FACE) else GL_CCW)
-            glEnable(GL_CULL_FACE)
+        # elif key == glfw.KEY_W:
+        #     glFrontFace(GL_CW if glGetBoolean(GL_CULL_FACE) else GL_CCW)
+        #     glEnable(GL_CULL_FACE)
 
 
 if __name__ == "__main__":
